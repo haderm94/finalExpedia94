442c7456422d77e9c27b3b911ade929cb52a5cfa
Mohammed Hader <Mohammed Hader>
2017-02-22 15:44:38 +0200

    Ready to deploy2

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -21,16 +21,43 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
 		JSONObject obj = new JSONObject("{interests : [{interestKey:Dogs}, {interestKey:Cats}]}");
+		try {
 
-List<String> list = new ArrayList<String>();
-JSONArray array = obj.getJSONArray("interests");
-for(int i = 0 ; i < array.length() ; i++){
-    list.add(array.getJSONObject(i).getString("interestKey"));
-	out.append(list.get(i).toString());
-}
-        out.write("hello heroku2".getBytes());
+        JSONParser jsonParser = new JSONParser();
+out.println("h1".getBytes());
+        File file = new File("C:\Users\haderk\Desktop\Expedia94\test\src\main\java\servlet\getOffers.json");
+out.println("h2".getBytes());
+        Object object = jsonParser.parse(new FileReader(file));
+
+        jsonObject = (JSONObject) object;
+
+        parseJson(jsonObject);
+
+    } catch (Exception ex) {
+        ex.printStackTrace();
+    }
+        //out.write("hello heroku2".getBytes());
         out.flush();
         out.close();
     }
+	public void parseJson(JSONObject jsonObject ,ServletOutputStream out) throws ParseException {
+
+    Set<Object> set = jsonObject.keySet();
+    Iterator<Object> iterator = set.iterator();
+    while (iterator.hasNext()) {
+        Object obj = iterator.next();
+        if (jsonObject.get(obj) instanceof JSONArray) {
+            out.println(obj.toString());
+            getArray(jsonObject.get(obj));
+        } else {
+            if (jsonObject.get(obj) instanceof JSONObject) {
+                parseJson((JSONObject) jsonObject.get(obj));
+            } else {
+                out.println(obj.toString() + "\t"
+                        + jsonObject.get(obj));
+				}
+			}
+		}
+	}
     
 }
