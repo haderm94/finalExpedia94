2fff563dc3dfe0b3ec8d2b26297f840fe6b70ca0
Mohammed Hader <Mohammed Hader>
2017-02-23 13:45:09 +0200

    Ready to deploy2

---
+++ src/main/java/servlet/HotelInfo.java
@@ -0,0 +1,44 @@
+public class HotelInfo{
+    String dest,tripDate,ratings,imgPath,description;
+	
+	
+    public String getDest() {
+        return dest;
+    }
+
+    public void setDest(String dest) {
+        this.dest = dest;
+    }
+
+    public String getTripDate() {
+        return tripDate;
+    }
+
+    public void setTripDate(String tripDate) {
+        this.tripDate = tripDate;
+    }
+
+    public String getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(String ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
